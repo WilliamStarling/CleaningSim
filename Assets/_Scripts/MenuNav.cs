7233e6baca92bf1e2591a5a6dc43165f26c51804
MD-3003948 <matthewdorlet@gmail.com>
2025-04-16 12:19:03 -0500

    Quiz Logic scripts
    I have begun working on the quiz logic and I am currently trying to get answer tracking working. This is still a WIP

--- Assets/_Scripts/MenuNav.cs
+++ Assets/_Scripts/MenuNav.cs
@@ -4,25 +4,88 @@ using UnityEngine;
 
 public class MenuNav : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Components")]
+    public int questionNumber = 1;
+    public string answerTag;
+    public GameObject Debug;
+
+    [Header("Question 1")]
+    public GameObject ButtonTrue1;
+    public GameObject ButtonFalse1;
+
+    [Header("Question 2")]
+    public GameObject ButtonA2;
+    public GameObject ButtonB2;
+    public GameObject ButtonC2;
+    public GameObject ButtonD2;
+
+    [Header("Question 3")]
+    public GameObject ButtonA3;
+    public GameObject ButtonB3;
+    public GameObject ButtonC3;
+    public GameObject ButtonD3;
+    public GameObject ButtonE;
+
+    [Header("Question 4")]
+    public GameObject ButtonA4;
+    public GameObject ButtonB4;
+    public GameObject ButtonC4;
+    public GameObject ButtonD4;
+
+    [Header("Question 5")]
+    public GameObject ButtonTrue5;
+    public GameObject ButtonFalse5;
+
+    private string Q1Answer;
+    private string Q2Answer;
+    private string Q3Answer;
+    private string Q4Answer;
+    private string Q5Answer;
 
     public void OpensScreen(GameObject next)
     {
+        Answer();
         next.SetActive(true);
+        questionNumber++;
     }
 
     public void CloseScreen(GameObject self)
     {
         self.SetActive(false);
+    }
+
+    public void Answer()
+    {
+        if (questionNumber == 1)
+        {
+            answerTag = this.tag;
+            Q1Answer = answerTag;
+        }
+        
+        else if (questionNumber == 2)
+        {
+
+        }
+        
+        else if (questionNumber == 3)
+        {
+
+        }
+        
+        else if (questionNumber == 4)
+        {
+
+        }
+        
+        else if (questionNumber == 5)
+        {
+
+        }
+
+        else
+        {
+
+        }
     }
 }
