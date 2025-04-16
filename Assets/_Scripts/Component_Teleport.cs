c165f2d9ac43fe115c7138a299416f7e7ac194a5
WilliamStarling <williamjonas@comcast.net>
2025-04-16 11:05:09 -0500

    Fixed it
    Added lines of code to reset both the velocity and angular velocity of each game object before it gets teleported.

--- Assets/_Scripts/Component_Teleport.cs
+++ Assets/_Scripts/Component_Teleport.cs
@@ -73,15 +73,23 @@ public class Component_Teleport : MonoBehaviour
         if (other.CompareTag("Component"))
         {
             Debug.Log("Component Dropped!");
+            Acetone.GetComponent<Rigidbody>().velocity = Vector3.zero; //Resets velocity
+            Acetone.GetComponent<Rigidbody>().angularVelocity = Vector3.zero; // Resets angular velocity
             Acetone.transform.position = new Vector3(Pos_Acetone_X, Pos_Acetone_Y, Pos_Acetone_Z);
             Acetone.transform.rotation = Quaternion.Euler(Rot_Acetone_X, Rot_Acetone_Y, Rot_Acetone_Z);
-
+            
+            Isopropyl.GetComponent<Rigidbody>().velocity = Vector3.zero; //Resets velocity
+            Isopropyl.GetComponent<Rigidbody>().angularVelocity = Vector3.zero; // Resets angular velocity
             Isopropyl.transform.position = new Vector3(Pos_Isopropyl_X, Pos_Isopropyl_Y, Pos_Isopropyl_Z);
             Isopropyl.transform.rotation = Quaternion.Euler(Rot_Isopropyl_X, Rot_Isopropyl_Y, Rot_Isopropyl_Z);
 
+            Methanol.GetComponent<Rigidbody>().velocity = Vector3.zero; //Resets velocity
+            Methanol.GetComponent<Rigidbody>().angularVelocity = Vector3.zero; // Resets angular velocity
             Methanol.transform.position = new Vector3(Pos_Methanol_X, Pos_Methanol_Y, Pos_Methanol_Z);
             Methanol.transform.rotation = Quaternion.Euler(Rot_Methanol_X, Rot_Methanol_Y, Rot_Methanol_Z);
 
+            AirHose.GetComponent<Rigidbody>().velocity = Vector3.zero; //Resets velocity
+            AirHose.GetComponent<Rigidbody>().angularVelocity = Vector3.zero; // Resets angular velocity
             AirHose.transform.position = new Vector3(Pos_Hose_X, Pos_Hose_Y, Pos_Hose_Z);
             AirHose.transform.rotation = Quaternion.Euler(Rot_Hose_X, Rot_Hose_Y, Rot_Hose_Z);
             AirHose.transform.Rotate(0, -90, 0);
