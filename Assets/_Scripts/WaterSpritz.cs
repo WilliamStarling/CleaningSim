501efb60a76d4ab0af0732d831c4227f9622387f
WilliamStarling <williamjonas@comcast.net>
2025-04-22 22:54:36 -0500

    commit
    commiting the changes done today so that Instructor Stacey can have the most up to date version, although I don't think any actual changes were made from the last commit.

---
+++ Assets/_Scripts/WaterSpritz.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Oculus.Interaction.HandGrab;
+using UnityEngine;
+using Oculus.Interaction;
+
+public class WaterSpritz : MonoBehaviour, IHandGrabUseDelegate
+{
+    public ParticleSystem waterSpritzEffect;
+    private float _dampedUseStrength = 0;
+    private float _lastUseTime;
+    private AnimationCurve _strengthCurve = AnimationCurve.EaseInOut(0f, 0f, 1f, 1f);
+    [SerializeField] private float _triggerSpeed = 3f;
+    public GrabInteractable grabInteractable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waterSpritzEffect = GetComponentInChildren<ParticleSystem>();
+        if (waterSpritzEffect == null)
+        {
+            Debug.LogError("WaterSpritz: ParticleSystem not found in children.");
+        }
+        grabInteractable = GetComponent<GrabInteractable>();
+        if (grabInteractable == null)
+        {
+            Debug.LogError("WaterSpritz: GrabInteractable not found on this GameObject.");
+        }
+    }
+    public void BeginUse()
+    {
+        _dampedUseStrength = 0f;
+        _lastUseTime = Time.realtimeSinceStartup;
+        // Logic to start using the water spritz
+        Debug.Log("Water spritz started!");
+        waterSpritzEffect.Play();
+    }
+    public void EndUse()
+    {
+
+    }
+    public float ComputeUseStrength(float strength)
+    {
+                    float delta = Time.realtimeSinceStartup - _lastUseTime;
+            _lastUseTime = Time.realtimeSinceStartup;
+            if (strength > _dampedUseStrength)
+            {
+                _dampedUseStrength = Mathf.Lerp(_dampedUseStrength, strength, _triggerSpeed * delta);
+            }
+            else
+            {
+                _dampedUseStrength = strength;
+            }
+            float progress = _strengthCurve.Evaluate(_dampedUseStrength);
+            return progress;
+    }
+}
